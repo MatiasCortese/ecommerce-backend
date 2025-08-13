1b8c24f426b01656d6f96f8d07ac0335e4e81307
Matias Cortese <matiasemanuelcortese@gmail.com>
2025-08-12 23:00:36 -0300

    testing cambio parseo manual

--- src/pages/api/auth/index.ts
+++ src/pages/api/auth/index.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 // import { sendCodeToEmail } from "@/lib/controllers/auth-controller";
-import { sendCodeToEmail } from "../../../lib/controllers/auth-controller";
+// import { sendCodeToEmail } from "../../../lib/controllers/auth-controller";
 
 type Data = {
   email: string;
@@ -21,12 +21,12 @@ if (typeof body === "string") {
 }
 console.log("SOY EL BODY ", body);
   if (req.method === 'POST') {
-    if(!req.body.email){
+    if(!body.email){
       res.status(400).json({ email: "Only email needed" });
       return;
     }
-    const { email, name, last_name, address, phone } = req.body;
-    await sendCodeToEmail(email, name, last_name, address, phone);
+    const { email, name, last_name, address, phone } = body;
+    // await sendCodeToEmail(email, name, last_name, address, phone);
     res.status(200).json({ message: "Email sended successfully"});
   } else {
     res.status(405).json({error: "Method Not Allowed"})

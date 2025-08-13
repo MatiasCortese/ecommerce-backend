3ddc1397a732beeb4aea95764b45038399e295c1
Matias Cortese <matiasemanuelcortese@gmail.com>
2025-08-12 22:28:55 -0300

    testing log

--- src/pages/api/auth/index.ts
+++ src/pages/api/auth/index.ts
@@ -10,8 +10,8 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>,
 ) {
+  console.log("SOY EL BODY ", req.body)
   if (req.method === 'POST') {
-    console.log("SOY EL BODY ", req.body)
     if(!req.body.email){
       res.status(400).json({ email: "Only email needed" });
       return;

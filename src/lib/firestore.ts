181a9e69143a9832bb327136ff165276df91c9ea
Matias Cortese <matiasemanuelcortese@gmail.com>
2025-08-15 13:57:26 -0300

    segmentamos vars de fb

--- src/lib/firestore.ts
+++ src/lib/firestore.ts
@@ -1,25 +1,24 @@
-// lib/firestore.ts o donde inicialices Firebase
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
-import admin from 'firebase-admin';
+
+import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
-  const serviceAccountRaw = process.env.FIREBASE_CONNECTION;
-
-  if (!serviceAccountRaw) {
-    throw new Error('FIREBASE_CONNECTION env variable is not defined');
-  }
-
-  const parsed = JSON.parse(serviceAccountRaw);
-
-  // Importante: reemplazar los \n por saltos de línea reales
-  if (parsed.private_key?.includes('\\n')) {
-    parsed.private_key = parsed.private_key.replace(/\\n/g, '\n');
-  }
-
   admin.initializeApp({
-    credential: admin.credential.cert(parsed),
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
   });
 }
 
 export const firestoreAdmin = admin.firestore();
-

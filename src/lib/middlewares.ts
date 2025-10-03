ae2d5871afa5f3d90cbb0975f6f208cb3c3b7a08
Matias Cortese <matiasemanuelcortese@gmail.com>
2025-10-03 15:49:31 -0300

    modificando temas de cors

--- src/lib/middlewares.ts
+++ src/lib/middlewares.ts
@@ -1,9 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import NextCors from "nextjs-cors";
 import parseBearerToken from "parse-bearer-token";
 import { decode } from "./jwt";
 
 export function authMiddleware(callback:any){
-    return function(req: NextApiRequest, res: NextApiResponse) {
+    return async function(req: NextApiRequest, res: NextApiResponse) {
+        await NextCors(req, res, {
+            methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+            origin: "*",
+            optionsSuccessStatus: 200,
+        });
+        if (req.method === "OPTIONS") {
+            res.status(200).json({});
+            return;
+        }
         const token = parseBearerToken(req);
         if (!token) {
             return res.status(401).json({ error: "Authorization header is required" });
